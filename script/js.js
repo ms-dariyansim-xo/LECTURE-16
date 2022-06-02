d129a90ff6fa295c7f53a29428acea4e427a61f3
ms-dariyansim-xo <103479356+ms-dariyansim-xo@users.noreply.github.com>
2022-06-02 10:18:50 -0700

    Update js.js

--- script/js.js
+++ script/js.js
@@ -1,1 +1,17 @@
-alert('TEST TO SEE IF THIS WORKS');
+//alert('TEST TO SEE IF THIS WORKS');
+
+//Variables
+const constant = 0;
+var dontDoVar = 5;
+let doLet = 10;
+
+//Conditions
+if(true){
+
+}
+else if(true){
+
+}
+else{
+
+}
