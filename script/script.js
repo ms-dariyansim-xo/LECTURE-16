146a8ab47b9e946b306561948d75b4a3e6b37d4c
ms-dariyansim-xo <103479356+ms-dariyansim-xo@users.noreply.github.com>
2022-06-02 13:18:49 -0700

    predraft, follow lecture from scratch

---
+++ script/script.js
@@ -0,0 +1,17 @@
+/* window.onload = function(){}
+
+const btnDisplay = document.getElementById();
+const numbers = document.getElementsByClassName();
+const selectColor = document.getElementById();
+const body = document.getElementsByTagName();
+
+btnDisplay.addEventListener('click', function(){
+    let selectedColorIndex = selectColor.selectedIndex;
+    let color = selectColor.option[selectedColorIndex].value;
+
+    body[0].style.backgroundColor = color;
+});
+
+*/
+
+
