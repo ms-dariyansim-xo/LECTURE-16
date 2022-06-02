1e013acea35f03b8441d7296614af07d99b3a279
ms-dariyansim-xo <103479356+ms-dariyansim-xo@users.noreply.github.com>
2022-06-02 13:40:24 -0700

    finalizing some details

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 //Testing to see if JS' alert works.
 //alert('This is working!');
 
-window.onload = function(){}
+window.onload = function(){
 
-const btnDisplay = document.getElementById();
-const numbers = document.getElementsByClassName();
-const selectColor = document.getElementById();
-const body = document.getElementsByTagName();
+const btnDisplay = document.getElementById("btnDisplay");
+const numbers = document.getElementsByClassName("number");
+const selectColor = document.getElementById("color");
+const body = document.getElementsByTagName("body");
 
 btnDisplay.addEventListener('click', function(){
     let selectedColorIndex = selectColor.selectedIndex;
@@ -15,6 +15,40 @@ btnDisplay.addEventListener('click', function(){
     body[0].style.backgroundColor = color;
 });
 
+}
+
+const constant = 0;
+var dontDoVar = 5;
+let doLet = 10;
+
+let isRunning = true;
+if(isRunning){
+
+
+}
+else if (true){
+
+}
+else
+{
+
+}
+
+switch("1"){
+    case "1":
+
+    break;
+    default:
+
+    break;
+}
+
+for(let i = 0; i <= 10; i++){
+
+}
+
+let sum = 0;
+const numbers = [43, 53, 66, 2]
 
 function AddTwoNumbers(num1, num2)
 {
@@ -23,6 +57,3 @@ function AddTwoNumbers(num1, num2)
 }
 
 let compareExample = 5 % 3;
-
-
-
